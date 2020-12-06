47395d427ae1328b24e637260be4bc25b78a0dfb
Carl Johnson <samirmohamed1981@gmail.com>
2020-12-06 18:11:55 +0300

    Commit Message xD

--- program2.py
+++ program2.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Como de undegradalos')
-
+    print('SAMP better than Roblox')
 
 if __name__ == '__main__':
     main()
